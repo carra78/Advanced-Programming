9ec3dc570c86ca289d2951efe8ff1ef3097d2e40
Carra78 <carra78@hotmail.com>
2017-11-29 12:17:07 +0000

    New console app created for CA1 solution

---
+++ CA1StreamReaderWriter/CA1StreamReaderWriter/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA1StreamReaderWriter
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+		}
+	}
+}
