3f5865de495ac4a615fd64a188dbf351654c3af5
Carra78 <carra78@hotmail.com>
2017-12-03 12:44:05 +0000

    Added class for commit object and class for commit object list

---
+++ CA1StreamReaderWriter/CA1StreamReaderWriter/Commit.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA1StreamReaderWriter
+{
+	class Commit
+	{
+		//local variables
+
+		//Properties
+		public string CommitRef { get; set; }
+		public string Author { get; set; }
+		public string CommitDate { get; set; }
+		public int CommitCommentLines { get; set; }
+		public string CommitComment { get; set; }
+		public string[] CommitChangedPaths { get; set; }
+
+		//Constructor(s)
+		public Commit()
+		{
+			//default construtor - temporarily included as overriden by addition of any other constructor 
+		}
+
+		public Commit(string cref, string auth, string date, int lines, string comment, string[] paths)
+		{
+			CommitRef = cref;
+			Author = auth;
+			CommitDate = date;
+			CommitCommentLines = lines;
+			CommitComment = comment;
+			CommitChangedPaths = paths;
+
+		}
+
+
+
+		//Method(s)
+		
+
+
+
+	}
+}
---
+++ CA1StreamReaderWriter/CA1StreamReaderWriter/CommitList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA1StreamReaderWriter
+{
+	class CommitList
+	{
+		//local varialbes
+		List<Commit> commitList = new List<Commit>();
+
+		//properties
+
+		//Constructor
+
+		//methods
+
+		//Add item to list
+		public void AddtoList(string cref, string auth, string date, int lines, string comment, string[] paths)
+		{
+			Commit c = new Commit(cref, auth, date, lines, comment, paths);
+			commitList.Add(c);
+		}
+	}
+}
